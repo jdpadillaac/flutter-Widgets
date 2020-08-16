0032870701b8c497b42b089cc9046b02bc04d70e
Jonathan <jonathan962911@gmail.com>
2020-08-16 00:44:36 -0500

    Inputs (textField ) tipo numero,  password o emial

--- lib/src/pages/inputs_page.dart
+++ lib/src/pages/inputs_page.dart
@@ -18,7 +18,14 @@ class _InputsPageState extends State<InputsPage> {
       ),
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-        children: <Widget>[_crearInput(), Divider(), _crearPersona()],
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearPersona(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword()
+        ],
       ),
     );
   }
@@ -46,5 +53,48 @@ class _InputsPageState extends State<InputsPage> {
 
   Widget _crearPersona() {
     return ListTile(title: Text('Nombre es: $_nombre'));
+  }
+
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.2)),
+          counter: Text('Cantidad de letras'),
+          hintText: 'Deberias ingresar esa vaina',
+          labelText: 'Nombre',
+          helperText: 'Solo es el nombre',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email)),
+      onChanged: (value) {
+        setState(() {
+          print(value);
+          _nombre = value;
+        });
+      },
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.2)),
+          hintText: 'El nombre de tu perro por ejemplo',
+          labelText: 'Contraseña',
+          helperText: 'Entre 2 y 10 caracteres',
+          suffixIcon: Icon(Icons.lock),
+          icon: Icon(Icons.alternate_email)),
+      onChanged: (value) {
+        setState(() {
+          print(value);
+          _nombre = value;
+        });
+      },
+    );
   }
 }
